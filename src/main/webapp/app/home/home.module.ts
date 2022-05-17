f7abed97c0bbf531f2adabd400af4f1a9c0586e4
maridene <medaziz.ridene@gmail.com>
2022-05-17 02:36:29 +0100

    improve home page

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,8 +5,10 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), NgbModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
